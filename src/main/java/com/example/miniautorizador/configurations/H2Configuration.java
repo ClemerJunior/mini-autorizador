1fd05ec78f128568a9a1bdd67820ee07c85a5a65
Clemer Junior <clemer.junior@hotmail.com>
2023-04-29 21:36:48 -0300

    configurações adicionais para o desenvolvimento

---
+++ src/main/java/com/example/miniautorizador/configurations/H2Configuration.java
@@ -0,0 +1,18 @@
+package com.example.miniautorizador.configurations;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.SQLException;
+
+@Configuration
+@Profile(value= "dev")
+public class H2Configuration {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+    }
+}
