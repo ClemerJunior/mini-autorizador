c50b00b41000c11584d15e4173e6bdc9931b1c9a
Clemer Junior <clemer.junior@hotmail.com>
2023-05-03 17:23:42 -0300

    refatorada a classe CartoesControllerTest para deixar o codigo mais limpo e os testes mais completos

--- src/test/java/com/example/miniautorizador/controller/CartoesControllerTest.java
+++ src/test/java/com/example/miniautorizador/controller/CartoesControllerTest.java
@@ -7,19 +7,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CartoesController.class)
 public class CartoesControllerTest {
@@ -34,95 +36,79 @@ public class CartoesControllerTest {
     private CartoesService cartoesService;
 
     @Test
-    @DisplayName("Requisição de criação do cartão correta, retornando 200")
-    void whenValidThenReturn200AndCreate() throws Exception {
+    @DisplayName("Requisição de criação do cartão correta, retornando 201")
+    void whenValidThenReturn201AndCreate() throws Exception {
         CartaoDTO cartaoDTO = new CartaoDTO();
         cartaoDTO.setNumeroCartao("1234564789");
         cartaoDTO.setSenha("senha");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        when(cartoesService.criarCartao(any())).thenReturn(cartaoDTO);
+
+        mockMvc.perform(post("/cartoes")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.numeroCartao").value("1234564789"))
+                .andExpect(jsonPath("$.senha").value("senha"));
+
+        verify(cartoesService, times(1)).criarCartao(any());
     }
 
     @Test
-    @DisplayName("Deve retornar 400 quando o numero do cartao invalido for nulo")
+    @DisplayName("Deve retornar 400 quando o numero do cartao invalido ou for nulo")
     void whenInvalidRequestNumeroCartaoNull() throws Exception {
         CartaoDTO cartaoDTO = new CartaoDTO();
         cartaoDTO.setNumeroCartao(null);
         cartaoDTO.setSenha("senha");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        mockMvc.perform(post("/cartoes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(status().isBadRequest());
+
+        verify(cartoesService, times(0)).criarCartao(any());
 
         cartaoDTO.setNumeroCartao("");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        mockMvc.perform(post("/cartoes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(status().isBadRequest());
+
+        verify(cartoesService, times(0)).criarCartao(any());
 
         cartaoDTO.setNumeroCartao("asdf15861");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        mockMvc.perform(post("/cartoes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(status().isBadRequest());
+
+        verify(cartoesService, times(0)).criarCartao(any());
     }
 
     @Test
-    @DisplayName("Deve retornar 400 quando o numero do cartao for vazio")
-    void whenInvalidRequestNumeroCartaoEmpty() throws Exception {
-        CartaoDTO cartaoDTO = new CartaoDTO();
-        cartaoDTO.setNumeroCartao("");
-        cartaoDTO.setSenha("senha");
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Deve retornar 400 quando o numero do cartao invalido")
-    void whenInvalidRequestNumeroCartaoInvalid() throws Exception {
-        CartaoDTO cartaoDTO = new CartaoDTO();
-        cartaoDTO.setNumeroCartao("1321asdfffa13215");
-        cartaoDTO.setSenha("senha");
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Deve retornar 400 quando a senha for nula")
+    @DisplayName("Deve retornar 400 quando a senha for nula ou vazia")
     void whenInvalidRequestSenhaNull() throws Exception {
         CartaoDTO cartaoDTO = new CartaoDTO();
         cartaoDTO.setNumeroCartao("1234569789");
         cartaoDTO.setSenha(null);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        mockMvc.perform(post("/cartoes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
-    }
+                .andExpect(status().isBadRequest());
 
-    @Test
-    @DisplayName("Deve retornar 400 quando a senha for vazia")
-    void whenInvalidRequestSenhaEmpty() throws Exception {
-        CartaoDTO cartaoDTO = new CartaoDTO();
-        cartaoDTO.setNumeroCartao("1234569789");
+        verify(cartoesService, times(0)).criarCartao(any());
+
         cartaoDTO.setSenha("");
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        mockMvc.perform(post("/cartoes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(status().isBadRequest());
+
+        verify(cartoesService, times(0)).criarCartao(any());
     }
 
     @Test
@@ -132,15 +118,17 @@ public class CartoesControllerTest {
         cartaoDTO.setNumeroCartao("1234569789");
         cartaoDTO.setSenha("senha");
 
-        Mockito.when(cartoesService.criarCartao(any())).thenReturn(cartaoDTO);
+        when(cartoesService.criarCartao(any())).thenReturn(cartaoDTO);
 
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        MvcResult mvcResult = mockMvc.perform(post("/cartoes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(cartaoDTO)))
                 .andReturn();
 
         String responseBody = mvcResult.getResponse().getContentAsString();
         Assertions.assertThat(responseBody).isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(cartaoDTO));
+
+        verify(cartoesService, times(1)).criarCartao(any());
     }
 
     @Test
@@ -150,32 +138,39 @@ public class CartoesControllerTest {
         cartaoDTO.setNumeroCartao("1234569789");
         cartaoDTO.setSenha("senha");
 
-        Mockito.when(cartoesService.criarCartao(any())).thenThrow(new CartaoExistenteException(cartaoDTO));
+        when(cartoesService.criarCartao(any())).thenThrow(new CartaoExistenteException(any()));
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartoes")
+        mockMvc.perform(post("/cartoes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(cartaoDTO)))
-                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+                .andExpect(status().isUnprocessableEntity());
+
+        verify(cartoesService, times(1)).criarCartao(any());
     }
 
     @Test
     @DisplayName("Deve retornar o saldo do cartao corretamente")
-    void shouldReturnSaldo() throws Exception{
-        Mockito.when(cartoesService.consultarSaldo(any())).thenReturn(new BigDecimal(500));
+    void shouldReturnSaldo() throws Exception {
+        when(cartoesService.consultarSaldo(any())).thenReturn(new BigDecimal("500"));
 
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/cartoes/1231456789")
+        MvcResult mvcResult = mockMvc.perform(get("/cartoes/1231456789")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andReturn();
+
         String responseBody = mvcResult.getResponse().getContentAsString();
 
-        Assertions.assertThat(new BigDecimal(500)).isEqualTo(new BigDecimal(responseBody));
+        Assertions.assertThat(new BigDecimal("500")).isEqualTo(new BigDecimal(responseBody));
+
+        verify(cartoesService, times(1)).consultarSaldo(any());
     }
 
     @Test
     @DisplayName("Deve retornar 400 quando o numero do cartao for invalido")
     void whenNumeroCartaoInvalid() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/cartoes/asdf121313")
+        mockMvc.perform(get("/cartoes/asdf121313")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(status().isBadRequest());
+
+        verify(cartoesService, times(0)).criarCartao(any());
     }
 }
